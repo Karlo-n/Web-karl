83bc3f5e7c6a72c984d58492b2e4bb23ef8c2e72
Karlo-n <yolorivera.5678@gmail.com>
2025-02-17 12:20:19 -0600

    Update index.js

--- app/utility/qrcode/index.js
+++ app/utility/qrcode/index.js
@@ -1,22 +1,17 @@
-const QRCode = require('qrcode');
+import { NowRequest, NowResponse } from '@vercel/node';
+import QRCode from 'qrcode';
 
-module.exports = async (req, res) => {
+export default async function (req = NowRequest, res = NowResponse) {
+    const { texto } = req.query;
+
+    if (!texto) {
+        return res.status(400).json({ error: "Falta el parámetro 'texto'" });
+    }
+
     try {
-        const { texto } = req.query;
-
-        if (!texto) {
-            return res.status(400).json({ error: "Falta el parámetro 'texto'" });
-        }
-
-        // Genera el código QR y devuelve un buffer en PNG
-        const qrImage = await QRCode.toBuffer(texto);
-
-        // Configura el encabezado de respuesta para una imagen PNG
-        res.setHeader('Content-Type', 'image/png');
-        res.send(qrImage);
-
+        const qr = await QRCode.toDataURL(texto);
+        res.json({ qr });
     } catch (error) {
-        console.error("Error generando el código QR:", error);
         res.status(500).json({ error: "Error generando el código QR" });
     }
-};
+}
